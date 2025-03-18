243d985cc8dd11b2b1b525ffabf893adb9be6a9c
Web3Kev <kidsfab.ipad@gmail.com>
2025-03-18 13:37:37 +0900

    button duck to top

--- src/app.tsx
+++ src/app.tsx
@@ -140,7 +140,7 @@ export function App() {
                 onClick={()=>handleSpawnDuck()}
                 style={{
                   position: "absolute",
-                  bottom: "50px",
+                  top: "50px",
                   left: "50%",
                   transform: "translateX(-50%)",
                   padding: "10px 20px",
